ea415ee7faa29b08bb313ac96907a394da20ef74
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-06-09 23:39:02 +0200

    add core app and add basic tests

--- app/app/randomstuff.py
+++
@@ -1,3 +0,0 @@
-def last_three_chars(s):
-  """Returns last three chars of a string"""
-  return s[-3:]
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+  def test_create_user_with_email_successful(self):
+    """test creating a new user with email is successfull"""
+    email = 'test@test.com'
+    password = 'Testing1234!'
+    user = get_user_model().objects.create_user(
+      email=email,
+      password=password
+    )
+
+    self.assertEqual(user.email, email)
+    self.assertTrue(user.check_password(password))
+  
+
+  def test_user_email_normalized(self):
+    """test email is normalized"""
+    email = 'test@TEST.cOM'
+    user = get_user_model().objects.create_user(
+      email,
+      'Testing1234!'
+    )
+
+    self.assertEqual(user.email, email.lower())
+  
+
+  def test_user_invalid_email(self):
+    """test creating user with no email raises error"""
+    with self.assertRaises(ValueError):
+      get_user_model().objects.create_user(None, 'Testing1234!')
+      
